01479c6ccf4afb6883b129ee8f79fd94bfc3a52d
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-05-31 19:03:56 -0300

    Atributos y Constructor

--- src/estructuras/PilaSobreArreglos.java
+++ src/estructuras/PilaSobreArreglos.java
@@ -6,6 +6,15 @@ package estructuras;
  */
 public class PilaSobreArreglos<T> implements Pila<T> {
 
+	private T[] elementos;
+	private int cantidad;
+	
+	public PilaSobreArreglos(int capacidadInicial) {
+	
+		elementos = (T[]) new Object[capacidadInicial];
+		cantidad = 0;
+	}
+
 	@Override
 	public boolean estaVacia() {
 
