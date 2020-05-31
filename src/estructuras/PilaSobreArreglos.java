2ea59683a275dc92c51624bd04e929280a3fe48b
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-05-31 18:58:36 -0300

    PilaSobreArreglos

---
+++ src/estructuras/PilaSobreArreglos.java
@@ -0,0 +1,32 @@
+package estructuras;
+
+/**
+ * Implementación de Pila (Stack) basada en arreglos.
+ * 
+ */
+public class PilaSobreArreglos<T> implements Pila<T> {
+
+	@Override
+	public boolean estaVacia() {
+
+		return false;
+	}
+
+	@Override
+	public void apilar(T nuevoElemento) {
+		
+	}
+
+	@Override
+	public T desapilar() {
+
+		return null;
+	}
+
+	@Override
+	public T obtenerTope() {
+
+		return null;
+	}
+
+}
