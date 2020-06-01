c474c2831817223a6fa6831a562ce72386ba3f49
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-05-31 21:54:01 -0300

    PilaSobreListaEnlazada

---
+++ src/estructuras/PilaSobreListaEnlazada.java
@@ -0,0 +1,31 @@
+package estructuras;
+
+/**
+ * Implementación de Pila (Stack) basada en una ListaEnlazada.
+ * 
+ */
+public class PilaSobreListaEnlazada<T> implements Pila<T> {
+
+	@Override
+	public boolean estaVacia() {
+
+		return false;
+	}
+
+	@Override
+	public void apilar(T nuevoElemento) {
+		
+	}
+
+	@Override
+	public T desapilar() {
+
+		return null;
+	}
+
+	@Override
+	public T obtenerTope() {
+
+		return null;
+	}
+}
