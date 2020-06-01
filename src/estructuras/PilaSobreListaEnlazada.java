f992fba19aff29ad555cd91f94483998e2428455
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-05-31 22:00:15 -0300

    Atributo y Constructor

--- src/estructuras/PilaSobreListaEnlazada.java
+++ src/estructuras/PilaSobreListaEnlazada.java
@@ -1,11 +1,20 @@
 package estructuras;
 
+import java.util.LinkedList;
+
 /**
  * Implementación de Pila (Stack) basada en una ListaEnlazada.
  * 
  */
 public class PilaSobreListaEnlazada<T> implements Pila<T> {
 
+	private LinkedList<T> elementos;
+	
+	public PilaSobreListaEnlazada() {
+
+		elementos = new LinkedList<T>();
+	}
+	
 	@Override
 	public boolean estaVacia() {
 
